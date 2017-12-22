2ecfbe165688260e9867f0efed87881b2a09e0e4
Kieran Campbell <kierancampbell@dhcp-206-12-52-224.eduroam.wireless.ubc.ca>
2017-12-22 13:29:33 -0800

    Possibly done for holidays

--- R/clonealign.R
+++ R/clonealign.R
@@ -22,10 +22,46 @@
 #' 
 clonealign <- function(gene_expression_data,
                        copy_number_data,
-                       s = NULL,
+                       size_factors = NULL,
                        max_iter = 100, 
                        rel_tol = 1e-5,
                        gene_filter_threshold = 0,
                        bp_param = bpparam()) {
+  
+  N <- NA # Number of cells
+  G <- NA # Number of genes
+  C <- NA # Number of clones
+  
+  # Parse gene expression data first
+  if(is(gene_expression_data, "SingleCellExperiment") || is(gene_expression_data, "SummarizedExperiment")) {
+    assay_names <- names(assays(gene_expression_data))
+    if(!("counts" %in% assay_names)) {
+      stop(paste("counts not in assays(gene_expression_data). Available assays:", paste(assay_names, collapse = ",")))
+    }
+    Y <- t(assay(gene_expression_data, "counts"))
+  } else if(is(gene_expression_data, "matrix")) {
+    Y <- gene_expression_data
+  } else {
+    stop("Input gene_expression_data must be SingleCellExperiment, SummarizedExperiment, or matrix")
+  }
+  N <- nrow(Y)
+  G <- ncol(Y)
+  
+  
+  # Parse size factors
+  if(is.null(size_factors) && if(is(gene_expression_data, "SingleCellExperiment"))) {
+    if(!is.null(sizeFactors(gene_expression_data))) {
+      size_factors <- sizeFactors(gene_expression_data)
+    }
+  }
+  
+  if(is.null(size_factors)) {
+    size_factors <- scran::computeSumFactors(t(Y))
+  }
+  
+  # Parse cnv data
+  
+  
+  
   
 }
